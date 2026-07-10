9fdb7ece405ff1a2fd6918d37b7cae980f84d63f
lizzezzie <lizezzie@gmail.com>
2026-07-10 20:08:34 +0300

    Excercise 4 completed

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,64 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+
+            string userInput = Console.ReadLine();
+            userNumber = int.Parse(userInput);
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+        //Compute sum
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum of the numbers is: {sum}");
+
+
+        //Compute average
+        float average = (float)sum / numbers.Count;
+        Console.WriteLine($"The average of the numbers is: {average}");
+
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The maximum number is: {max}");
+
+        //Find the largest number
+        int largest = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+        Console.WriteLine($"The largest number is: {largest}");
+
+        //Find the smallest positive number
+        int smallestPositive = int.MaxValue;
+        foreach (int number in numbers)
+        {
+            if (number > 0 && number < smallestPositive)
+            {
+                smallestPositive = number;
+            }
+        }
+        Console.WriteLine($"The smallest positive number is: {smallestPositive}");
     }
 }
